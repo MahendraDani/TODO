0b8c49a39716d4a581c3013b901daca31df1ce07
Mahendra Dani <danimahendra0904@gmail.com>
2023-07-06 11:40:51 +0530

    init client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-ellipsis font-bond text-4xl text-orange-600">
+        Hello World again again !
+      </div>
+    </>
+  );
+}
+
+export default App;
