a2babae631e0a5c4209d61f8e2c7051e87ae115e
Mahendra Dani <danimahendra0904@gmail.com>
2023-07-11 09:56:50 +0530

    feat: linked backend with signup form

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -3,75 +3,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-
-  // const handleForm = async () => {
-  //   try {
-  //     const response = await axios.post("http://localhost:3000/users/login", {
-  //       email,
-  //       password,
-  //     });
-  //     const token = response.data.accessToken;
-  //     const userId = response.data.userId;
-  //     console.log(token);
-  //     localStorage.setItem("token", token);
-  //     const userIdExists = localStorage.getItem("userId");
-  //     if (!userIdExists) {
-  //       localStorage.setItem(localStorage.setItem("userId", userId));
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const logout = () => {
-  //   localStorage.removeItem("token");
-  //   localStorage.removeItem("userId");
-  // };
-  // return (
-  //   <div>
-  //     <div>
-  //       <div className="m-4">
-  //         <label htmlFor="email">Eamil</label>
-  //         <input
-  //           type="text"
-  //           name="email"
-  //           className="border-2 border-black px-2"
-  //           onChange={(e) => setEmail(e.target.value)}
-  //         />
-  //       </div>
-  //       <div className="m-4">
-  //         <label htmlFor="password">Password</label>
-  //         <input
-  //           type="text"
-  //           name="password"
-  //           className="border-2 border-black px-2"
-  //           onChange={(e) => setPassword(e.target.value)}
-  //         />
-  //       </div>
-  //       <div>
-  //         <button
-  //           type="submit"
-  //           className="px-3 py-1 rounded-full bg-sky-400 hover:bg-sky-600"
-  //           onClick={handleForm}
-  //         >
-  //           Login
-  //         </button>
-  //       </div>
-  //     </div>
-
-  //     <br />
-  //     <button
-  //       type="submit"
-  //       className="px-3 py-1 rounded-full bg-sky-400 hover:bg-sky-600"
-  //       onClick={logout}
-  //     >
-  //       Logout
-  //     </button>
-  //   </div>
-  // );
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -80,29 +11,16 @@ const Signup = () => {
 
   const handleSingup = async (e) => {
     try {
-      e.prevenDefault();
-      // const response = await axios.post("http://localhost:3000/users/signup", {
-      //   firstName,
-      //   lastName,
-      //   email,
-      //   password: confirmPassword,
-      // });
-
-      // console.log(response);
-      const response = await fetch("http://localhost:3000/users/signup", {
-        method: "POST",
-        body: JSON.stringify({
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: confirmPassword,
-        }),
-        headers: {
-          "Content-type": "Application/json",
-        },
+      e.preventDefault();
+      const response = await axios.post("http://localhost:3000/users/signup", {
+        firstName,
+        lastName,
+        email,
+        password: confirmPassword,
       });
-      const data = await response.json();
-      console.log(data);
+
+      const userId = response.data.id;
+      alert(response.data.message);
     } catch (error) {
       console.log(error);
     }
@@ -113,10 +31,7 @@ const Signup = () => {
         <div className="absolute top-0 left-0 right-0 w-full h-16 bg-slate-200 flex items-center justify-center">
           <h1 className="text-3xl font-bold text-purple-800">TODO</h1>
         </div>
-        <form
-          className="bg-white shadow-md shadow-purple-200 flex flex-col justify-between items-center gap-6 p-4 rounded-sm text-gray-500 md:text-lg"
-          onSubmit={handleSingup}
-        >
+        <form className="bg-white shadow-md shadow-purple-200 flex flex-col justify-between items-center gap-6 p-4 rounded-sm text-gray-500 md:text-lg">
           <div>
             <h1 className="text-2xl font-bold text-purple-700">
               CREATE YOUR ACCOUNT
@@ -175,7 +90,7 @@ const Signup = () => {
           <div>
             <button
               className="border-2 border-purple-700 px-5 text-lg text-purple-700 rounded-sm hover:bg-purple-700 hover:text-slate-200 ease-in duration-300 font-semibold"
-              type="submit"
+              onClick={handleSingup}
             >
               SIGNUP
             </button>
