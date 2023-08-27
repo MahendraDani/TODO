b1003cfde36c09769052dd2a7d69485ba1227651
Mahendra Dani <danimahendra0904@gmail.com>
2023-08-27 10:57:34 +0530

    feat: todos can be deleted only if user is found in database

--- server/controllers/todos/deleteTodo.js
+++ server/controllers/todos/deleteTodo.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { statusCodes } = require("../../constants/statuscodes");
 const { getTodoIndexFromId } = require("../../constants/getTodoIndexFromId");
 const { removeTodoById } = require("../../constants/removeTodoById");
+const { getIndexFromId } = require("../../constants/getIndexFromId");
 
 const TODOS_DIRECTORY = path.join(
   __dirname,
@@ -22,26 +23,37 @@ const USERS_DIRECTORY = path.join(
   "users.json"
 );
 const deleteTodo = async (req, res) => {
-  const todoId = req.params._id;
-  if (!todoId) {
-    await res
-      .status(statusCodes.BAD_REQUEST)
-      .json({ message: "Todo Id is required!" });
+  const userId = req.headers.id;
+  if (!userId) {
+    res.status(statusCodes.FORBIDDEN).json({ message: "userID is required!" });
   } else {
-    const todoData = fs.readFileSync(TODOS_DIRECTORY, "utf8");
-    let TODOS = await JSON.parse(todoData);
-
-    const todoIndex = getTodoIndexFromId(TODOS, todoId);
-    console.log(todoIndex);
-    res.send();
-    if (todoIndex === -1) {
-      await res
-        .status(statusCodes.FORBIDDEN)
-        .json({ message: "Invalid or incorrect todo id" });
+    const USERS = await JSON.parse(fs.readFileSync(USERS_DIRECTORY, "utf8"));
+    const userIndex = getIndexFromId(USERS, userId);
+    if (userIndex === -1) {
+      return res
+        .status(statusCodes.NOT_FOUND)
+        .json({ message: "User not found!" });
     } else {
-      TODOS = removeTodoById(TODOS, todoId);
-      fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
-      await res.status(200).json({ message: "Todo deleted successfully!" });
+      const todoId = req.params._id;
+      if (!todoId) {
+        await res
+          .status(statusCodes.BAD_REQUEST)
+          .json({ message: "Todo Id is required!" });
+      } else {
+        const todoData = fs.readFileSync(TODOS_DIRECTORY, "utf8");
+        let TODOS = await JSON.parse(todoData);
+
+        const todoIndex = getTodoIndexFromId(TODOS, todoId);
+        if (todoIndex === -1) {
+          await res
+            .status(statusCodes.FORBIDDEN)
+            .json({ message: "Invalid or incorrect todo id" });
+        } else {
+          TODOS = removeTodoById(TODOS, todoId);
+          fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
+          await res.status(200).json({ message: "Todo deleted successfully!" });
+        }
+      }
     }
   }
 };
