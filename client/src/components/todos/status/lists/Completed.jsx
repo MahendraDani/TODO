463bf7af3c30abe5b31551649f763ee1b8991f0f
Mahendra Dani <danimahendra0904@gmail.com>
2023-09-21 21:32:13 +0530

    feat: added pagination for completed todo lists

--- client/src/components/todos/status/lists/Completed.jsx
+++ client/src/components/todos/status/lists/Completed.jsx
@@ -7,6 +7,8 @@ import DeleteTodoWarning from "../../delete/DeleteTodoWarning";
 import UpdateTodo from "../../update/UpdateTodo";
 import CreateTodos from "../../create/CreateTodos";
 import { IoGridOutline } from "react-icons/io5";
+import ReactPaginate from "react-paginate";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
 const CompletedTodos = ({ todos, status, handleCompletedTodosCards }) => {
   const [selectedTodoId, setSelectedTodoId] = useState("");
@@ -28,6 +30,71 @@ const CompletedTodos = ({ todos, status, handleCompletedTodosCards }) => {
   const closeModal = () => {
     showCreateTodoModal ? setShowCreateTodoModal(false) : null;
   };
+
+  const [pageNumber, setPageNumber] = useState(0);
+  const todosPerPage = 8;
+  const pagesVisited = pageNumber * todosPerPage;
+  const pageCount = Math.ceil(completedTodos.length / todosPerPage);
+  const displayTodos = completedTodos
+    .slice(pagesVisited, pagesVisited + todosPerPage)
+    .map((todo, index) => {
+      return todo.status === status ? (
+        <div
+          key={index}
+          onClick={() => {
+            setSelectedTodoId(todo._id);
+          }}
+          className={`flex flex-row justify-start items-center ${
+            index == 0
+              ? `border-t-[1.6px] border-b-[1.6px] hover:border-t-[1.6px] hover:border-slate-50`
+              : `border-b-[1.6px]`
+          }  p-2 gap-2 border-slate-50 hover:bg-slate-200 ease-in duration-200 cursor-default odd:bg-slate-50`}
+        >
+          <div className="min-w-[2.5rem] max-w-[2rem] text-center text-slate-400">
+            {index + 1}
+          </div>
+          <div className="max-w-[35rem] min-w-[35rem] ">
+            <h2 className="flex justify-start items-center text-slate-600 mb-1 gap-2 ">
+              <div className="hover:text-sky-400 hover:cursor-pointer">
+                {todo.title}
+              </div>
+              <div
+                className={`font-normal w-2 h-2 rounded-full text-sm ${
+                  todo.status === COMPLETED
+                    ? "bg-green-400"
+                    : null || todo.status === NOT_STARTED
+                    ? "bg-red-400"
+                    : null || todo.status === IN_PROGRESS
+                    ? "bg-yellow-400"
+                    : null
+                }`}
+              ></div>
+            </h2>
+            <p className=" text-slate-400 mr-2 text-sm">{todo.description}</p>
+          </div>
+          <p className="max-w-[6rem] min-w-[6rem] mr-2 text-slate-400 text-sm">
+            {todo.createdOn}
+          </p>
+          <p className="max-w-[6rem] min-w-[6rem] text-slate-400 text-sm">
+            {todo.createdAt}
+          </p>
+
+          <div className="grid place-content-center min-w-[2rem] max-w-[2rem] pr-7">
+            <UpdateTodo todoId={selectedTodoId} todos={todos} />
+          </div>
+          <div>
+            <div className="text-sm max-w-[2rem] min-w-[2rem]">
+              <DeleteTodoWarning todoId={selectedTodoId} />
+            </div>
+          </div>
+        </div>
+      ) : null;
+    });
+
+  const onPageChange = ({ selected }) => {
+    setPageNumber(selected);
+  };
+  // Main component
   return completedTodos.length === 0 ? (
     <div className="h-[80vh] grid place-content-center">
       <div className="flex flex-col gap-3 justify-center items-center">
@@ -84,61 +151,26 @@ const CompletedTodos = ({ todos, status, handleCompletedTodosCards }) => {
           <div className="max-w-[2rem] min-w-[2rem]"></div>
         </div>
         <div>
-          {completedTodos.map((todo, index) => {
-            return todo.status === status ? (
-              <div
-                key={index}
-                onClick={() => {
-                  setSelectedTodoId(todo._id);
-                }}
-                className={`flex flex-row justify-start items-center ${
-                  index == 0
-                    ? `border-t-[1.6px] border-b-[1.6px] hover:border-t-[1.6px] hover:border-slate-50`
-                    : `border-b-[1.6px]`
-                }  p-2 gap-2 border-slate-50 hover:bg-slate-200 ease-in duration-200 cursor-default odd:bg-slate-50`}
-              >
-                <div className="min-w-[2.5rem] max-w-[2rem] text-center text-slate-400">
-                  {index + 1}
-                </div>
-                <div className="max-w-[35rem] min-w-[35rem] ">
-                  <h2 className="flex justify-start items-center text-slate-600 mb-1 gap-2 ">
-                    <div className="hover:text-sky-400 hover:cursor-pointer">
-                      {todo.title}
-                    </div>
-                    <div
-                      className={`font-normal w-2 h-2 rounded-full text-sm ${
-                        todo.status === COMPLETED
-                          ? "bg-green-400"
-                          : null || todo.status === NOT_STARTED
-                          ? "bg-red-400"
-                          : null || todo.status === IN_PROGRESS
-                          ? "bg-yellow-400"
-                          : null
-                      }`}
-                    ></div>
-                  </h2>
-                  <p className=" text-slate-400 mr-2 text-sm">
-                    {todo.description}
-                  </p>
-                </div>
-                <p className="max-w-[6rem] min-w-[6rem] mr-2 text-slate-400 text-sm">
-                  {todo.createdOn}
-                </p>
-                <p className="max-w-[6rem] min-w-[6rem] text-slate-400 text-sm">
-                  {todo.createdAt}
-                </p>
-
-                <div className="grid place-content-center min-w-[2rem] max-w-[2rem] pr-7">
-                  <UpdateTodo todoId={selectedTodoId} todos={todos} />
-                </div>
-                <div>
-                  <div className="text-sm max-w-[2rem] min-w-[2rem]">
-                    <DeleteTodoWarning todoId={selectedTodoId} />
-                  </div>
-                </div>
+          <div className="relative">{displayTodos}</div>
+          <ReactPaginate
+            previousLabel={
+              <div>
+                <BsArrowLeft />
               </div>
-            ) : null;
-          })}
+            }
+            nextLabel={
+              <div>
+                <BsArrowRight />
+              </div>
+            }
+            pageCount={pageCount}
+            onPageChange={onPageChange}
+            containerClassName={
+              "absolute bottom-5 flex justify-center items-center gap-1"
+            }
+            activeLinkClassName="bg-slate-300"
+            pageLinkClassName="px-3 py-1 bg-slate-100 hover:bg-slate-200 ease-in duration-100"
+          />
         </div>
       </section>
     </div>
