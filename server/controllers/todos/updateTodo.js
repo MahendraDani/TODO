4c8baad8a69b7b1676625cf20c0db4c8f9bf0898
Mahendra Dani <danimahendra0904@gmail.com>
2023-10-01 12:20:50 +0530

    feat: Added database in update todo route

--- server/controllers/todos/updateTodo.js
+++ server/controllers/todos/updateTodo.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+const { Todo } = require("../../models/todos/Todos");
+const { User } = require("../../models/users/Users");
 const { statusCodes } = require("../../constants/globals/statuscodes");
 const { getIndexFromId } = require("../../constants/globals/getIndexFromId");
 const {
@@ -36,31 +38,44 @@ const USERS_DIRECTORY = path.join(
 const updateTodo = async (req, res) => {
   try {
     // check userID from header
-    const userId = req.headers.id;
+    const userId = req.params.id;
+    const todoId = req.params.todoId;
     if (!userId) {
       return res
         .status(statusCodes.BAD_REQUEST)
         .json({ message: "User Id not found!" });
     }
 
-    const USERS = await JSON.parse(fs.readFileSync(USERS_DIRECTORY, "utf8"));
-    const userIndex = getIndexFromId(USERS, userId);
-    if (userIndex === -1) {
-      return res
-        .status(statusCodes.FORBIDDEN)
-        .json({ message: "Invalid or incorrect user ID" });
-    }
-
-    const todoId = req.params.todoId;
     if (!todoId) {
       return res
         .status(statusCodes.BAD_REQUEST)
         .json({ message: "TodoId not fonud" });
     }
 
-    let TODOS = await JSON.parse(fs.readFileSync(TODOS_DIRECTORY, "utf8"));
-    const todoIndex = getTodoIndexFromId(TODOS, todoId);
-    if (todoIndex === -1) {
+    // const USERS = await JSON.parse(fs.readFileSync(USERS_DIRECTORY, "utf8"));
+    // const userIndex = getIndexFromId(USERS, userId);
+    // if (userIndex === -1) {
+    //   return res
+    //     .status(statusCodes.FORBIDDEN)
+    //     .json({ message: "Invalid or incorrect user ID" });
+    // }
+    const user = await User.findOne({ id: userId });
+    if (!userId) {
+      return res
+        .status(statusCodes.FORBIDDEN)
+        .json({ message: "Invalid or incorrect user ID" });
+    }
+
+    // let TODOS = await JSON.parse(fs.readFileSync(TODOS_DIRECTORY, "utf8"));
+    // const todoIndex = getTodoIndexFromId(TODOS, todoId);
+    // if (todoIndex === -1) {
+    //   return res
+    //     .status(statusCodes.FORBIDDEN)
+    //     .json({ message: "Invalid or incorrect todo Id" });
+    // }
+
+    const todo = await Todo.findOne({ todoId });
+    if (!todo) {
       return res
         .status(statusCodes.FORBIDDEN)
         .json({ message: "Invalid or incorrect todo Id" });
@@ -69,42 +84,71 @@ const updateTodo = async (req, res) => {
     const { newTitle, newDescription, newStatus } = req.body;
     if (newTitle && !newDescription) {
       // Only update title
-      TODOS = updateTodoTitleById(TODOS, todoId, newTitle);
-      fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
-      return res.status(statusCodes.SUCCESS).json({
-        message: "Todo updated successfully",
-        todo: TODOS[todoIndex],
-      });
+      // TODOS = updateTodoTitleById(TODOS, todoId, newTitle);
+      // fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
+      // return res.status(statusCodes.SUCCESS).json({
+      //   message: "Todo updated successfully",
+      //   todo: TODOS[todoIndex],
+      // });
+      await Todo.findOneAndUpdate(
+        { todoId },
+        {
+          title: newTitle,
+        }
+      );
+      const newTodo = await Todo.findOne({ todoId });
+      return res.status(statusCodes.SUCCESS).json({ todo: newTodo });
     }
 
     if (!newTitle && newDescription) {
       // only update description
-      TODOS = updateTodoDescriptionById(TODOS, todoId, newDescription);
-      fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
-      return res
-        .status(statusCodes.SUCCESS)
-        .json({ message: "Todo updated successfully", todo: TODOS[todoIndex] });
+      // TODOS = updateTodoDescriptionById(TODOS, todoId, newDescription);
+      // fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
+      // return res
+      //   .status(statusCodes.SUCCESS)
+      //   .json({ message: "Todo updated successfully", todo: TODOS[todoIndex] });
+      await Todo.findOneAndUpdate({ todoId }, { description: newDescription });
+      const newTodo = await Todo.findOne({ todoId });
+      return res.status(statusCodes.SUCCESS).json({ todo: newTodo });
     }
 
     if (newTitle && newDescription && newStatus) {
       // update both title and description
-      TODOS = updateTodoTitleById(TODOS, todoId, newTitle);
-      TODOS = updateTodoDescriptionById(TODOS, todoId, newDescription);
-      TODOS = updateTodoStatusById(TODOS, todoId, newStatus);
-      fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
-      return res.status(statusCodes.SUCCESS).json({
-        message: "Todo updated successfully",
-        todo: TODOS[todoIndex],
-      });
+      // TODOS = updateTodoTitleById(TODOS, todoId, newTitle);
+      // TODOS = updateTodoDescriptionById(TODOS, todoId, newDescription);
+      // TODOS = updateTodoStatusById(TODOS, todoId, newStatus);
+      // fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
+      // return res.status(statusCodes.SUCCESS).json({
+      //   message: "Todo updated successfully",
+      //   todo: TODOS[todoIndex],
+      // });
+      await Todo.findOneAndUpdate(
+        { todoId },
+        {
+          title: newTitle,
+          description: newDescription,
+          status: newStatus,
+        }
+      );
+      const newTodo = await Todo.findOne({ todoId });
+      return res.status(statusCodes.SUCCESS).json({ todo: newTodo });
     }
 
     if (newStatus) {
-      TODOS = updateTodoStatusById(TODOS, todoId, newStatus);
-      fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
-      return res.status(statusCodes.SUCCESS).json({
-        message: "Todo updated successfully",
-        todo: TODOS[todoIndex],
-      });
+      // TODOS = updateTodoStatusById(TODOS, todoId, newStatus);
+      // fs.writeFileSync(TODOS_DIRECTORY, JSON.stringify(TODOS));
+      // return res.status(statusCodes.SUCCESS).json({
+      //   message: "Todo updated successfully",
+      //   todo: TODOS[todoIndex],
+      // });
+      await Todo.findOneAndUpdate(
+        { todoId },
+        {
+          status: newStatus,
+        }
+      );
+      const newTodo = await Todo.findOne({ todoId });
+      return res.status(statusCodes.SUCCESS).json({ todo: newTodo });
     }
   } catch (error) {
     console.log(error);
--- server/routes/todos/updateTodo.js
+++ server/routes/todos/updateTodo.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { updateTodo } = require("../../controllers/todos/updateTodo");
 
-router.put("/update/:todoId", updateTodo);
+router.put("/update/:id/:todoId", updateTodo);
 
 module.exports = router;
