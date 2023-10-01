0685586dc46aa567f2b4ff8e23ab1ac0add0d090
Mahendra Dani <danimahendra0904@gmail.com>
2023-10-01 11:18:58 +0530

    feat: added database to get all todos of a user route

--- server/routes/todos/getTodos.js
+++ server/routes/todos/getTodos.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { getTodos } = require("../../controllers/todos/getTodos");
 
-router.get("/todos", getTodos);
+router.get("/todos/all/:id", getTodos);
 
 module.exports = router;
